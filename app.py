ce8a4af86ed26a8c41e1f061b11b34e7703cd06c
gustavo <guwotocoski@gmail.com>
2025-09-16 16:24:38 -0300

    implement css and fixed bugs

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect, url_for, flash
+=======
+from flask import Flask, render_template, request, redirect, url_for
+>>>>>>> 673114b (implement css and fixed bugs)
 from game import Personagem, RAÇAS, CLASSES, DistribuidorAtributos
 
 app = Flask(__name__)
@@ -20,6 +24,7 @@ def create():
         classe_nome = request.form["classe"]
         estilo = request.form.get("estilo", "classico")
 
+<<<<<<< HEAD
         # selecionar atributos (por enquanto tudo clássico)
         if estilo == "classico":
             atributos = DistribuidorAtributos.estilo_classico()
@@ -38,16 +43,51 @@ def create():
 
         flash(f"Personagem {nome} criado com sucesso!")
         return redirect(url_for("list_personagens"))
+=======
+        raca = RAÇAS.get(raca_nome)
+        classe = CLASSES.get(classe_nome)
+
+        if estilo == "classico":
+            atributos = DistribuidorAtributos.estilo_classico()
+            p = Personagem(nome, atributos, raca, classe)
+            p.salvar()
+            return redirect(url_for("list_personagens"))
+
+        elif estilo in ["aventureiro", "heroico"]:
+            # Gerar os valores correspondentes
+            if estilo == "aventureiro":
+                valores = DistribuidorAtributos.rolar_aventureiro()
+            else:
+                valores = DistribuidorAtributos.rolar_heroico()
+
+            # Renderizar o template distribuir.html
+            return render_template(
+                "distribuir.html",
+                nome=nome,
+                raca=raca_nome,
+                classe=classe_nome,
+                estilo=estilo,
+                valores=valores,
+                atributos=["Força","Destreza","Constituição","Inteligência","Sabedoria","Carisma"]
+            )
+>>>>>>> 673114b (implement css and fixed bugs)
 
     return render_template("create.html", racas=RAÇAS.keys(), classes=CLASSES.keys())
 
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 673114b (implement css and fixed bugs)
 # ----------------- VER PERSONAGEM -----------------
 @app.route("/personagem/<nome>")
 def personagem(nome):
     p = Personagem.carregar(nome)
     if not p:
+<<<<<<< HEAD
         flash("Personagem não encontrado!")
+=======
+>>>>>>> 673114b (implement css and fixed bugs)
         return redirect(url_for("list_personagens"))
     return render_template("personagem.html", personagem=p)
 
@@ -59,5 +99,36 @@ def list_personagens():
     return render_template("list.html", personagens=personagens)
 
 
+<<<<<<< HEAD
+=======
+
+
+
+
+@app.route("/distribuir", methods=["POST"])
+def distribuir():
+    nome = request.form["nome"]
+    raca_nome = request.form["raca"]
+    classe_nome = request.form["classe"]
+    estilo = request.form["estilo"]
+
+    atributos = {}
+    for attr in ["Força","Destreza","Constituição","Inteligência","Sabedoria","Carisma"]:
+        valor_str = request.form.get(attr)
+        if not valor_str:
+            return redirect(request.referrer)
+        atributos[attr] = int(valor_str)
+
+    raca = RAÇAS.get(raca_nome)
+    classe = CLASSES.get(classe_nome)
+
+    p = Personagem(nome, atributos, raca, classe)
+    p.salvar()
+
+    return redirect(url_for("list_personagens"))
+
+
+
+>>>>>>> 673114b (implement css and fixed bugs)
 if __name__ == "__main__":
     app.run(debug=True)
